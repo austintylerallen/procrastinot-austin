97564133cf2e169c8106d25afcc7ade7af611d6b
austinallen <austintallen07@gmail.com>
2024-06-28 22:44:20 -0400

    adding ssl=true to the env file as well

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For self-signed certificates
+    },
+  },
   logging: false, // Disable logging; set to true to enable or provide a function for custom logging
 });
 
